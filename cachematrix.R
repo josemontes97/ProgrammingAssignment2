7a98d569ee5f1960ad34e7025c4cd42220ebe9a2
Jose <josemontes1@gmail.com>
2016-08-16 17:46:32 -0500

    Assignment2 grading excersice

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,5 @@
 ## This script consists of two functions namely,
+
 ##1. makeCacheMatrix() - This function creates a special matrix 
 ##object that can cache its inverse.
 ##2. cacheSolve()      - This function computes inverse of the special 
